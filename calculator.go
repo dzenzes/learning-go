2226e2216ca19d83d8cdd6b28a57ada68ee30bff
Daniel Mies <daniel.mies@codecentric.de>
2020-11-09 18:47:53 +0100

    chapter 2+3

--- calculator.go
+++ calculator.go
@@ -3,11 +3,11 @@ package calculator
 
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
 }
