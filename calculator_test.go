3fec398262cf9995adec4bc3124616f2883d855f
Daniel Mies <daniel.mies@codecentric.de>
2020-11-09 18:52:11 +0100

    chapter 4: multiply

--- calculator_test.go
+++ calculator_test.go
@@ -22,3 +22,11 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 32
+	got := calculator.Multiply(6, 7)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
