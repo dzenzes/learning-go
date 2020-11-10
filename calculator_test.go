897f63b38c73ea0ce9ad4f63711368a17408a48d
Daniel Mies <daniel.mies@codecentric.de>
2020-11-10 10:15:15 +0100

    chapter 7

--- calculator_test.go
+++ calculator_test.go
@@ -2,13 +2,15 @@ package calculator_test
 
 import (
 	"calculator"
+	"math/rand"
 	"testing"
 )
 
 type testCase struct {
-	a, b float64
-	want float64
-	name string
+	a, b        float64
+	want        float64
+	name        string
+	errExpected bool
 }
 
 func TestAdd(t *testing.T) {
@@ -26,6 +28,20 @@ func TestAdd(t *testing.T) {
 			t.Errorf("%s: Add(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
 		}
 	}
+}
+
+func TestAddRandom(t *testing.T) {
+	t.Parallel()
+	for i := 0; i < 100; i++ {
+		a := rand.Float64()
+		b := rand.Float64()
+		want := a + b
+		got := calculator.Add(a, b)
+		if want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", a, b, want, got)
+		}
+	}
+
 }
 
 func TestSubtract(t *testing.T) {
@@ -57,5 +73,31 @@ func TestMultiply(t *testing.T) {
 		if tc.want != got {
 			t.Errorf("%s: Multiply(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
 		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 4, b: 2, want: 2, name: "Normal example with positive numbers"},
+		{a: 1, b: -1, want: -1, name: "Divide with one negative number"},
+		{a: -2, b: -4, want: 0.5, name: "Two negative numbers result in positive result"},
+		{a: 4, b: 0, errExpected: true, name: "Shouldn'T be possible to divide by zero"},
+	}
+
+	for _, tc := range testCases {
+
+		got, err := calculator.Divide(tc.a, tc.b)
+
+		errReceived := err != nil
+		if tc.errExpected != errReceived {
+			t.Fatalf("Divide(%f, %f): unexpected error status: %v", tc.a, tc.b, errReceived)
+		}
+
+		if !tc.errExpected && tc.want != got {
+			t.Errorf("%s: Divide(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
+		}
+		// The error value was as you expected, and the data value also matched expectation (pass)
+
 	}
 }
